e731f8d6a2691ad3bc6078380be8308044e29eb6
guoqiang <guoqiang.zhu@ubixai.com>
2026-08-14 15:31:41 +0800

    release: package UBiMAXBanner 1.5.2.11

---
+++ UBiMAXBanner.xcframework/ios-arm64/UBiMAXBanner.framework/Headers/UBiMAXBanner-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UMTAdManager+Banner.h"
+#import "UMTBanner.h"
+
+FOUNDATION_EXPORT double UBiMAXBannerVersionNumber;
+FOUNDATION_EXPORT const unsigned char UBiMAXBannerVersionString[];
+
